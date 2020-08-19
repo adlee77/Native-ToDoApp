206e7f6595afaefb58dfb33b1bc39e853290b52e
Andrew Lee <andrewimg@gmail.com>
2020-08-19 12:29:36 -0700

    added first header component

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import Header from './Components/Header';
 
-export default function App() {
+export default class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      todoInput: '',
+      todoArray: [
+        {id: 0, title: '1st Task', done: false},
+        {id: 1, title: '2nd Task', done: false}
+      ]
+    }
+  }
+  render() {
+    const statusbar = (Platform.OS == "ios") ? <View style={styles.statusbar}></View> : <View></View>;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {statusbar}
+      <Header />      
     </View>
   );
+}
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
+  statusbar: {
+    backgroundColor: 'red',
+    height: 20
+  }
 });
